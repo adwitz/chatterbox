72c74bb8109673c14581e24aaa18f1605ced5481
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2013-10-07 19:45:29 -0700

    write fetch, display  messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,39 @@
 // YOUR CODE HERE:
+/*
+
+doc ready func
+get request- jquery get, ajax
+setInterval 'refresh'
+append messages <ul>
+
+HTML
+create in DOM- textbox, send button
+
+*/
+
+$(document).ready(function(){
+  var serverData;
+  $.get('https://api.parse.com/1/classes/chatterbox?order=createdAt', function(data){
+    fetch(data);
+  });
+
+  var fetch = function(data){
+    var lastTen = [];
+    for (var i=data.results.length-1; i>data.results.length-11; i--){
+      lastTen.push(data.results[i]);
+    }
+    display(lastTen);
+  };
+
+  var display = function(msgs){
+    var messageHTML;
+    for (var i=0; i<msgs.length; i++){
+      messageHTML = '<li>' + '<b>' + msgs[i].username + ': ' + '</b>' + msgs[i].text + '</li>';
+      $(".messages").append(messageHTML);
+    }
+    
+  };
+
+
+
+});

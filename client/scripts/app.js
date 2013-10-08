8cc02951ddc2ce65925fc60bdc1083b43c7706e4
adwitz <adam.witzel@gmail.com>
2013-10-08 11:51:09 -0700

    adds and joins new room

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -13,60 +13,69 @@ create in DOM- textbox, send button
 
 $(document).ready(function(){
   var serverData;
-  var currentRoom = ""//"The HackPad";    //  ASK: OK to make this global?
+  var currentRoom = ""//"The HackPad";    //  ASK: OK to make this global? 
+
+  var User = function(currentRoom){
+    this.username = location.search.split("=")[1];
+    this.friends = [];
+    this.currentRoom = currentRoom || "";
+  }
+
+  User.prototype.createRoom = function(name){
+    this.currentRoom = name;
+  }
+
+  var user = new User(currentRoom);
 
   var fetchAndDisplayWrapper = function(){
     $.get('https://api.parse.com/1/classes/chatterbox?order=-createdAt', function(data){
       fetch(data);
-
-    // $.ajax({
-    //   url: 'https://api.parse.com/1/classes/chatterbox',
-    //   type: 'GET',
-    //   data: '',
-    //   contentType: 'application/json',
-    //   order: 'createdAt'
-    // };
-
     })
   };
 
   fetchAndDisplayWrapper();
   setInterval(fetchAndDisplayWrapper
-    //function(){$.get('https://api.parse.com/1/classes/chatterbox?order=createdAt', 
-    //function(data){
-    //  fetch(data)})}
   , 5000);
 
-  $('.send').on('click', function(){
+  $('.send.message').on('click', function(){
     var text = $('#textbox').val();
     text = htmlEscape(text);
     sendMessage(text);
-    console.log(text);
+    $("#textbox").val("");
   });
 
-  var sendMessage = function(str){
+  $('.send.room').on('click', function(){
+    var name = $('#newRoom').val();
+    name = htmlEscape(name);
+    user.createRoom(name);
+    $('#newRoom').val('');
+  });
+
+  
+
+  var sendMessage = function(msg){
     var message = {
       'username': location.search.split("=")[1],
-      'text': str,
-      'roomname': currentRoom
+      'text': msg,
+      'roomname': user.currentRoom
     };
 
     console.log(message);
 
     $.ajax({
-    // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message');
-  }
-});
+      // always use this url
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+      }
+    });
   };
 
   var htmlEscape = function(str) {
@@ -79,15 +88,14 @@ $(document).ready(function(){
     };
   
   var fetch = function(data){
-    var lastTen = [];
-    for (var i=0; i<10; i++){
-      lastTen.push(data.results[i]);
+    var roomMsgs = [];
+    for (var i=0; i<data.results.length && roomMsgs.length < 10; i++){
+      if(user.currentRoom === "" || data.results[i]["roomname"] === user.currentRoom){
+        roomMsgs.push(data.results[i]);
+      }
+     
     }
-
-    // for (var i=data.results.length-1; i>data.results.length-11; i--){
-    //   lastTen.push(data.results[i]);
-    // }
-    display(lastTen);
+    display(roomMsgs);
   };
 
   var display = function(msgs){
@@ -101,5 +109,7 @@ $(document).ready(function(){
   };
 
 
-
 });
+
+
+
